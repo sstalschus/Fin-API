af0a4c1e6fcc815fa892907b45ed18cc60a7f42e
Samuel <samuel@buzzlead.com.br>
2021-03-11 19:46:49 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## FinAPI - Financeira
+
+API Financeira desenvolvida em Node, apenas para fins de aprendizado.
+
+---
+
+### Requisitos
+
+- [x] Deve ser possível criar uma conta
+- [] Deve ser possível buscar o extrato bancário do cliente
+- [] Deve ser possível realizar um depósito
+- [] Deve ser possível realizar um saque
+- [] Deve ser possível buscar o extrato do cliente por data
+- [] Deve ser possível obter dados da conta do cliente
+- [] Deve ser possível obter dados da conta do cliente
+- [] Deve ser possível deletar uma conta
+
+---
+
+## Regras de negócio
+
+- [x] Não deve cadastrar uma conta com CPF existente
+- [X] Não deve ser possível fazer depósito em uma conta não existente
+- [] Não deve ser possível buscar extrato, fazer saque, ou excluir uma conta inexistente
+- [] Não é possível sacar dinheiro de uma conta sem saldo
---
+++ src/index.js
@@ -0,0 +1,101 @@
+const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+
+const app = express()
+
+app.use(express.json())
+
+const customers = []
+
+// Middleware
+function verifyIfExistsAccountCPF(req, res, next) {
+    const { cpf } = req.headers
+
+    const customer = customers.find(customer =>
+        customer.cpf === cpf)
+
+    if (!customer) {
+        return res.status(400).json({ error: 'Customer not found' })
+    }
+
+    req.customer = customer
+
+    return next()
+}
+
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === 'credit') {
+            return acc + operation.amount
+        } else acc - operation.amount
+    }, 0)
+
+    return balance
+}
+
+app.post('/account', (req, res) => {
+    const { cpf, name } = req.body
+
+    const customerAlreadyExists = customers.some((customer) =>
+        customer.cpf === cpf)
+
+    if (customerAlreadyExists) {
+        return res.status(400).json({ error: 'Customer already exists!' })
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    })
+
+    return res.status(201).send()
+})
+
+app.get('/statement/', verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req
+    return res.json(customer.statement)
+})
+
+app.post('/deposit', verifyIfExistsAccountCPF, (req, res) => {
+    const { description, amount } = req.body
+
+    const { customer } = req
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: 'credit'
+    }
+
+    customer.statement.push(statementOperation)
+
+    return res.status(201).send()
+})
+
+app.post('/withdraw', verifyIfExistsAccountCPF, (req, res) => {
+    const { amount } = req.body
+    const { customer } = req
+
+    const balance = getBalance(customer.statement)
+
+    if (balance < amount) {
+        return res.status(400).json({ error: 'Insufficient founds!' })
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: 'debit'
+    }
+
+    customer.statement.push(statementOperation)
+
+    return res.status(201).send()
+})
+
+app.listen(3333, () => {
+    console.log('Server is running!!!')
+})
