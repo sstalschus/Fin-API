0e63355e0e06ee2fa6ce56ff4cb414c322baf6a6
Samuel <samuel@buzzlead.com.br>
2021-03-11 21:54:38 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ function getBalance(statement) {
     const balance = statement.reduce((acc, operation) => {
         if (operation.type === 'credit') {
             return acc + operation.amount
-        } else acc - operation.amount
+        } else return acc - operation.amount
     }, 0)
 
     return balance
@@ -94,6 +94,48 @@ app.post('/withdraw', verifyIfExistsAccountCPF, (req, res) => {
     customer.statement.push(statementOperation)
 
     return res.status(201).send()
+})
+
+app.get('/statement/date', verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req
+    const { date } = req.query
+
+    const dateFormat = new Date(date + " 00:00")
+
+    const statement = customer.statement.filter((statement) =>
+        statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+    return res.json(statement)
+})
+
+app.put('/account', verifyIfExistsAccountCPF, (req, res) => {
+    const { name } = req.body
+    const { customer } = req
+
+    customer.name = name
+
+    return res.status(201).send()
+})
+
+app.get('/account', verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req
+
+    return res.json(customer)
+})
+
+app.delete('/account', verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req
+
+    customers.splice(customer, 1)
+
+    return res.status(200).json(customers)
+})
+
+app.get('/balance', verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req
+    const balance = getBalance(customer.statement)
+
+    return res.json(balance)
 })
 
 app.listen(3333, () => {
